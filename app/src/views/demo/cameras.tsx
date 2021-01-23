79324a6dc77971a6b3371649f4bfccd646267483
daobee <chjr93820@163.com>
2021-01-23 09:01:24 +0800

    注释、背景

--- app/src/views/demo/cameras.tsx
+++ app/src/views/demo/cameras.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Table, Popconfirm, Upload, Input, Space } from 'antd'
+// TODO: 替换为remixicon图标
 import { SearchOutlined } from '@ant-design/icons'
 
 import { withStore } from '@/src/components'
@@ -14,15 +15,18 @@ import {
   FilterDropdownProps,
 } from 'antd/lib/table/interface'
 
-type EditableTableProps = Parameters<typeof Table>[0]
-
+/**
+ * 组件Props的interface
+ */
 interface CamerasPageProps extends PageProps, StoreProps {
   cameraCount: StoreStates['cameraCount']
   cameraList: StoreStates['cameraList']
 }
 
+/**
+ * 组件State的interface
+ */
 declare interface CamerasPageState {
-  resData: Partial<queryTestInfoUsingGET.Response>
   currentFocusing: any
   currentUploading: Camera[] | null
   currentPage: number
@@ -32,52 +36,64 @@ declare interface CamerasPageState {
   sortKeyPre: string | null
   sortOrderPre: string | null
   filters: Record<string, React.Key[] | null> | null
+  filtersPre: Record<string, React.Key[] | null> | null
   uploadFileList: any[]
-  loading: boolean
-  createWindowLoading: boolean
   asyncDispatchLoading: boolean
 }
 
+/**
+ * Table组件Column的配置type
+ */
+type EditableTableProps = Parameters<typeof Table>[0]
 type ColumnTypes = Exclude<EditableTableProps['columns'], undefined>
 
 /**
- * CamerasPageProps 是组件的 props 类型声明
- * CamerasPageState 是组件的 state 类型声明
- * props 和 state 的默认值需要单独声明
+ * Cameras页，展示、管理摄像机列表
+ * TODO: 剥离indexdedDB逻辑
  */
 @withStore(['cameraList', 'cameraCount'])
 export default class Cameras extends React.Component<CamerasPageProps, CamerasPageState> {
-  // state 初始化
+  /**
+   * state 初始化默认值
+   */
   state: CamerasPageState = {
-    resData: {},
-    currentFocusing: null,
-    currentUploading: null,
-    currentPage: 1,
-    pageSize: 5,
-    sortKey: null,
-    sortOrder: null,
-    sortKeyPre: null,
-    sortOrderPre: null,
-    filters: null,
-    uploadFileList: [],
-    loading: false,
-    createWindowLoading: false,
-    asyncDispatchLoading: false,
+    currentFocusing: null, // 当前操作的row
+    currentUploading: null, // 当前上传的json
+    currentPage: 1, // 当前页码
+    pageSize: 10, // 当前每页显示条数
+    sortKey: null, // 当前根据哪一列排序
+    sortOrder: null, // 当前为升序或降序
+    sortKeyPre: null, // 之前根据哪一列排序
+    sortOrderPre: null, // 之前为升序或降序
+    filters: null, // 当前筛选条件
+    filtersPre: null, // 之前筛选条件
+    uploadFileList: [], // 上传文件列表，目前没用
+    asyncDispatchLoading: false, // 同步操作加载状态
   }
 
-  // 构造函数
+  /**
+   * 构造函数
+   * @param props
+   */
   constructor(props: CamerasPageProps) {
     super(props)
   }
 
+  /**
+   * 组件生命周期：componentDidMount
+   */
   componentDidMount(): void {
+    // 组件加载时进行首次摄像机查询
     this.props.dispatch(this.queryCameraListPage)
   }
 
+  /**
+   * render开始
+   */
   render(): JSX.Element {
-    // const { resData, loading, createWindowLoading } = this.state
     const { currentPage, pageSize, asyncDispatchLoading, uploadFileList } = this.state
     const { cameraList, cameraCount } = this.props
+    // Table Column列属性配置
     const columns = [
       {
         title: 'UUID',
@@ -117,7 +133,7 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
       total: cameraCount,
       pageSize: pageSize,
       showSizeChanger: true,
-      pageSizeOptions: ['5', '9', '20'],
+      pageSizeOptions: ['5', '10', '20'],
     }
     return (
       <div className="cameras layout-padding">
@@ -175,9 +191,16 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
           onChange={this.handleTableChange}
         />
       </div>
-    )
-  }
+    ) // return() ends
+  } // render ends
 
+  /**
+   * 处理Table状态变化事件：分页、筛选、排序，会引起重新进行摄像机列表查询
+   * @param pagination
+   * @param filters
+   * @param sorter
+   * @param extra
+   */
   handleTableChange = (
     pagination: TablePaginationConfig,
     filters: Record<string, React.Key[] | null>,
@@ -185,12 +208,13 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
     extra: TableCurrentDataSource<Camera>
   ): void => {
     switch (extra.action) {
+      /** 翻页或修改每页显示条数 */
       case 'paginate':
-        // 翻页或修改每页显示条数操作
         this.setState({ currentPage: pagination.current ?? 1, pageSize: pagination.pageSize ?? 5 }, () => {
           this.props.dispatch(this.queryCameraListPage)
         })
         break
+      /** 排序 */
       case 'sort':
         if (!Array.isArray(sorter))
           this.setState(
@@ -203,6 +227,7 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
             }
           )
         break
+      /** 筛选 */
       case 'filter':
         this.setState({ filters: filters }, () => this.props.dispatch(this.queryCameraListPage))
         break
@@ -210,26 +235,28 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
         console.log('Unhandlded table action:', extra.action)
         break
     }
-  }
+  } // function handleTableChange ends
+
   /**
    * 摄像机查询
    * 从indexdedDB查询摄像机列表分页及设备总数，放到react store以供显示
    */
   queryCameraListPage = async (dispatch: Dispatch): Promise<void> => {
+    /** 开始查询，列表进入loading状态 */
     this.setState({ asyncDispatchLoading: true })
     try {
-      /** 应用筛选和排序条件 */
+      /** 排序和筛选开始 */
       // 创建Collection
       let cameraCollection: Collection<Camera, number> = $db.cameras.toCollection()
-      // 排序
-      const { sortKey, sortOrder, sortKeyPre, sortOrderPre } = this.state
-      if (!(sortKey == sortKeyPre && sortOrder == sortOrderPre)) {
+      // 排序,如果排序或关键字搜索发生改变，则需要重排，否则跳过排序
+      const { sortKey, sortOrder, sortKeyPre, sortOrderPre, filters, filtersPre } = this.state
+      if (!(sortKey == sortKeyPre && sortOrder == sortOrderPre) || filters !== filtersPre) {
         if (sortKey && sortOrder == 'ascend') cameraCollection = await $db.cameras.orderBy(sortKey)
         if (sortKey && sortOrder == 'descend') cameraCollection = await $db.cameras.orderBy(sortKey).reverse()
         this.setState({ sortKeyPre: sortKey, sortOrderPre: sortOrder })
       } else console.log('skiping sort')
       // 关键词搜索
-      const { filters } = this.state
+      // TODO: 进行类型断言，提高filter效率，目前全部按string处理
       cameraCollection = cameraCollection.filter((camera) => {
         let flag = true
         if (filters) {
@@ -241,14 +268,17 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
         }
         return flag
       })
-      /** 排序、过滤结束 */
+      if (filtersPre !== filters) this.setState({ filtersPre: filters })
+      /** 排序、筛选结束 */
+      /** 收尾工作开始 */
       // 更新查询结果总数
       const count: number = await cameraCollection.count()
       dispatch({ type: 'ACTION_SET_CAMERACOUNT', data: count })
-      // 如果设备被删除后，当前页码不再存在，则回到最后一页
+      // 如果总数变化，新的总页数小于当前页码，则回到最后一页
       const { pageSize, currentPage } = this.state
       const maxPage = Math.ceil(count / pageSize)
       if (maxPage < currentPage) {
+        // TODO: 将setState回调写法改为同步写法，否则列表的loading状态会先于查询完成而改变
         this.setState({ currentPage: maxPage }, async () => {
           // 切出分页
           cameraCollection = cameraCollection.offset((maxPage - 1) * pageSize).limit(pageSize)
@@ -260,16 +290,18 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
         cameraCollection = cameraCollection.offset((currentPage - 1) * pageSize).limit(pageSize)
         // 执行查询
         dispatch({ type: 'ACTION_SET_CAMERALISTPAGE', data: await cameraCollection.toArray() })
+        /** 收尾工作结束 */
       }
     } catch (error) {
       console.error(error)
     }
+    /** 结束查询，列表结束loading状态 */
     this.setState({ asyncDispatchLoading: false })
-  }
+  } // function queryCameraListPage ends
 
   /**
-   * 添加摄像机
-   * 添加camera到indexdedDB
+   * 添加随机摄像机
+   * 添加camera到indexdedDB，用于测试
    */
   addCameraToDB = async (dispatch: Dispatch): Promise<void> => {
     this.setState({ asyncDispatchLoading: true })
@@ -319,7 +351,7 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
   }
 
   /**
-   * 替换Upload上传行为，读取本地json，放到内存
+   * 替换Upload组件默认上传行为，读取本地json至内存
    * @param options
    */
   customRequest = (options: RcCustomRequestOptions): void => {
@@ -382,7 +414,7 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
    */
   searchInput: Input | null = null
   /**
-   * 生成筛选按钮及弹窗
+   * 动态生成筛选按钮及弹窗
    * @param dataIndex
    */
   getColumnSearchProps = (dataIndex: string): Record<string, unknown> => ({
@@ -419,10 +451,6 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
       </div>
     ),
     filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
-    // onFilter: (value: string | number | boolean, record: Camera) =>
-    //   record[dataIndex]
-    //     ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
-    //     : '',
     onFilterDropdownVisibleChange: (visible: boolean) => {
       if (visible) {
         setTimeout(() => {
@@ -430,5 +458,5 @@ export default class Cameras extends React.Component<CamerasPageProps, CamerasPa
         }, 100)
       }
     },
-  })
+  }) // function getColumnSearchProps ends
 } // class Cameras end
