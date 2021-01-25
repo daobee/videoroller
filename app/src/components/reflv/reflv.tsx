9e8b8133328ee289ebe8bc36eec8b4b54da3d2ca
daobee <chjr93820@163.com>
2021-01-25 22:49:27 +0800

    warp flvjs

---
+++ app/src/components/reflv/reflv.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import FlvJs from 'flv.js'
+
+import './reflv.less'
+
+interface Props extends FlvJs.MediaDataSource {
+  className?: string
+  style?: unknown
+  config?: FlvJs.Config
+}
+
+interface State {
+  path: string
+}
+
+export class Reflv extends React.Component<Props, State> {
+  flvPlayer?: FlvJs.Player
+
+  componentWillUnmount(): void {
+    if (this.flvPlayer) {
+      this.flvPlayer.unload()
+      this.flvPlayer.detachMediaElement()
+    }
+  }
+
+  render(): JSX.Element {
+    const { className, style } = this.props
+    return (
+      <video
+        className={className}
+        controls={true}
+        style={Object.assign(
+          {
+            width: '100%',
+          },
+          style
+        )}
+        ref={this.initFlv}
+      />
+    )
+  }
+
+  initFlv = ($video: HTMLVideoElement): void => {
+    if ($video) {
+      if (FlvJs.isSupported()) {
+        const flvPlayer = FlvJs.createPlayer({ ...this.props }, this.props.config)
+        flvPlayer.attachMediaElement($video)
+        flvPlayer.load()
+        flvPlayer.play()
+        this.flvPlayer = flvPlayer
+      }
+    }
+  }
+} // class Reflv end
